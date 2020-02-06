e48ff3a3507aa88c9f71d18145b1aad72e2361fa
Artur Pedrosa <maverickanp@gmail.com>
2020-02-06 09:18:11 -0300

    feat: create recipe model schema for mongodb

---
+++ src/models/recipe.js
@@ -0,0 +1,30 @@
+import mongoose, { Schema } from 'mongoose';
+import timestamps from 'mongoose-timestamp';
+import { composeWithMongoose } from 'graphql-compose-mongoose';
+
+export const RecipeSchema = new Schema(
+    {
+        name: {
+            type: String,
+            required: true,
+        },
+        category: {
+            type: String,
+            required: true,
+        },
+        description: {
+            type: String,
+            required: true,
+        }
+    },
+    {
+        collection: 'recipes',
+    }
+);
+
+RecipeSchema.plugin(timestamps);
+
+RecipeSchema.index({ createdAt: 1, updatedAt: 1 });
+
+export const Recipe = mongoose.model('Recipe', RecipeSchema);
+export const RecipeTC = composeWithMongoose(Recipe);
